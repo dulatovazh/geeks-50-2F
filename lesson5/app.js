255af06e07a28c391d2db0843aa09d3d14fdba7a
dulatovazh <dulatovazh@gmail.com>
2025-05-23 01:19:58 +0300

    DZ5-final

--- lesson5/app.js
+++ lesson5/app.js
@@ -1,95 +1,19 @@
-console.log ('Hello');
+console.log('Hello');
 
-let customer = 'John';
-let customerName = 'Bakyt';
-let customer_name = 'Kuban';
-let customerSurname = 'Sariev';
-let $ = '12'
-let $num = 12
-let num123 = 123
-let age = 18
+let light = prompt('Укажите цвет светофора');
+console.log(light);
+console.log(typeof light);
 
-console.log(`Имя=${customer} Фамилия=${customerSurname} Возраст=${age}`);
+let red = 'красный';
+let yellow = 'желтый';
+let green = 'зеленый';
 
-console.log('Имя=', customer, ' ', 'Фамилия', customerSurname, 'Возраст=', age);
-console.log('ОАО "Северэлектро"')
-let file = 'd:\\data\\profile.txt'
-console.log(file)
-let number = 123
-let number1 = 3.14
-console.log(number)
-number = 500
-console.log(number)
-
-// console.log(a+b, '+')
-// console.log(a-b, '-')
-// console.log(a*b, '*')
-// console.log(a/b, '/')
-// console.log(a%b, '%')
-// console.log(a**b, '**')
-// console.log(a%b, '%')
-// console.log(645+'so')
-let a = 10
-let b = 5
-console.log(a > b, 'a>b')
-console.log(a<b, 'a<b')
-console.log(a<=b, 'a<=b')
-console.log(a==b, 'a==b')
-console.log(a !== b, 'a!==b')
-console.log(a != b, 'a!=b')
-console.log(true && false, '&& и')
-console.log(true || false, '|| или')
-console.log(true, 'Не')
-console.log(!(true && false) && true)
-console.log('0' || 'answer')
-console.log(0 || 'awda')
-let x=5
-x = x+3
-console.log(x)
-x+= 3
-console.log(x)
-x-=3
-console.log(x)
-x/= 3
-console.log(x)
-x*=3
-console.log(x)
-let y = 1
-console.log(++y)
-console.log(--y)
-console.log(y)
-let by = 1
-console.log (++by)
-console.log (++by)
-
-console.log (typeof y)
-console.log (typeof 'y')
-console.log (typeof undefined)
-console.log (typeof null)
-let df
-console.log (df)
-let empty = null
-console.log(typeof empty)
-
-let age1 = prompt ('сколько вам лет?')
-console.log(age1)
-console.log(typeof age1)
-
-if (age1>1000) {
-    console.log('Вы бессметрный')
-} else if (age1>18) {
-    console.log('Вы совершеннолетний')
-} else if (age1>0) {
-    console.log ('Вы не совершеннолетний')
+if (light === red) {
+    console.log('Остановитесь!');
+} else if (light === yellow) {
+    console.log('Приготовьтесь!');
+} else if (light === green) {
+    console.log('Можно ехать!');
 } else {
-    console.log('Вы не правильно указали возраст')
+    console.log('Ошибка: некорректный цвет светофора.');
 }
-
-
-
-
-
-
-
-
-
